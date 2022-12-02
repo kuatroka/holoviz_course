e676ad001c0c238f1844958b97fe489351bafa44
kuatroka <9093546+kuatroka@users.noreply.github.com>
2022-12-02 01:56:45 +0000

    .

--- holoviz-master/examples/tutorial/earthquake_data.py
+++ holoviz-master/examples/tutorial/earthquake_data.py
@@ -4,19 +4,19 @@ import calendar
 import datetime as dt
 import requests
 
-URL = "https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime={start}&endtime={end}&minmagnitude=2.0&orderby=time"
+# URL = "https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime={start}&endtime={end}&minmagnitude=2.0&orderby=time"
 
-for yr in range(2000, 2023):
-    for m in range(1, 13):
-        if os.path.isfile("{yr}_{m}.csv".format(yr=yr, m=m)):
-            continue
-        _, ed = calendar.monthrange(yr, m)
-        start = dt.datetime(yr, m, 1)
-        end = dt.datetime(yr, m, ed, 23, 59, 59)
-        with open("{yr}_{m}.csv".format(yr=yr, m=m), "w", encoding="utf-8") as f:
-            f.write(
-                requests.get(URL.format(start=start, end=end)).content.decode("utf-8")
-            )
+# for yr in range(2000, 2023):
+#     for m in range(1, 13):
+#         if os.path.isfile("{yr}_{m}.csv".format(yr=yr, m=m)):
+#             continue
+#         _, ed = calendar.monthrange(yr, m)
+#         start = dt.datetime(yr, m, 1)
+#         end = dt.datetime(yr, m, ed, 23, 59, 59)
+#         with open("{yr}_{m}.csv".format(yr=yr, m=m), "w", encoding="utf-8") as f:
+#             f.write(
+#                 requests.get(URL.format(start=start, end=end)).content.decode("utf-8")
+#             )
 
 dfs = []
 for i in range(2000, 2022):
@@ -24,11 +24,14 @@ for i in range(2000, 2022):
         if not os.path.isfile("%d_%d.csv" % (i, m)):
             continue
         df = pd.read_csv(
-            "%d_%d.csv" % (i, m), dtype={"nst": "float64"}, error_bad_lines=False
+            "%d_%d.csv" % (i, m),
+            dtype={"nst": "float64"},
+            error_bad_lines=False,
+            delimiter=",",
         )
         dfs.append(df)
 df = pd.concat(dfs, sort=True)
-df.to_parquet("../earthquakes.parq", "fastparquet")
+df.to_parquet("./earthquakes.parq", "fastparquet")
 
 # Reprojected, cleaned and gzip (not snappy)
 
